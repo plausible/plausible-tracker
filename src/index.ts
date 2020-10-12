045fc561bfc4fca812cac1f918521b2e91e78e59
Maronato <gustavomaronato@gmail.com>
2020-10-12 19:52:16 -0300

    refactor(index): make index exports explicit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export * from './lib/tracker';
+import Plausible from './lib/tracker';
+export type { PlausibleOptions } from './lib/tracker';
+export type { EventOptions } from './lib/request';
+
+export default Plausible;
